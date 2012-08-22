d88718bf7912d0b604704b96a625f799c79995be
Matt Raibert <mattraibert@gmail.com>
2012-08-22 15:35:33 -0400

    oops, should replace numbers AND stars incase things got re-ordered

--- src/sorty_util.rb
+++ src/sorty_util.rb
@@ -1,8 +1,9 @@
+MD_LIST_ITEM = /^(\*|\d+\.) /
+
 def replace_stars_with_ordinal_numbers (items)
-  items.each_with_index.map { |x, i| x.sub /\*/, "#{i + 1}." }
+  items.each_with_index.map { |x, i| x.sub MD_LIST_ITEM, "#{i + 1}. " }
 end
 
-MD_LIST_ITEM = /^(\*|\d+\.) /
 
 def choose_item_lines(lines)
   lines.select { |line| MD_LIST_ITEM =~ line }
--- test/sorty_util_test.rb
+++ test/sorty_util_test.rb
@@ -3,8 +3,8 @@ require 'sorty_util'
 
 class SortyUtilTest < MiniTest::Unit::TestCase
   def test_replace_stars
-    assert_equal(["1. this is one", "2. this is another"],
-                 replace_stars_with_ordinal_numbers(["* this is one", "* this is another"]))
+    assert_equal(["1. this is one", "2. this is another", "3. card molly"],
+                 replace_stars_with_ordinal_numbers(["* this is one", "* this is another", "99. card molly"]))
   end
 
   def test_choose_item_lines
