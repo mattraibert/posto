5de7a99df20fa5687bbb89f05c079ac25c3970d4
Matt Raibert <mattraibert@gmail.com>
2012-08-22 15:54:09 -0400

    rewrite todo file

--- src/sorty.rb
+++ src/sorty.rb
@@ -5,7 +5,7 @@ def ask_human_to_compare(x, y)
   while true do
     puts "1. #{hide_markdown x}"
     puts "2. #{hide_markdown y}"
-    response = ask "type which one is more important (1, 2 or just hit enter if you don't care): "
+    response = ask "which one is more important? (1, 2 or just hit enter if you don't care): "
     return 0 if response.to_i == 0
     return -1 if response.to_i == 1
     return 1 if response.to_i == 2
@@ -14,4 +14,12 @@ end
 
 items = choose_item_lines(IO.read("todo.md").split("\n"))
 items = items.sort { |x, y| compare_sorted_items(x,y) or ask_human_to_compare(x, y) }
-puts replace_stars_with_ordinal_numbers(items)
+items = replace_stars_with_ordinal_numbers(items)
+puts items
+IO.write("todo.md",
+         """
+todo
+----
+
+#{items.join("\n")}
+""")
