e5b53d6c0c57e8c9137c76737a1f2bfc23ac8d06
Matt Raibert <mattraibert@gmail.com>
2012-09-12 16:20:42 -0400

    always print the return value of app methods

--- lib/posto/application.rb
+++ lib/posto/application.rb
@@ -11,11 +11,11 @@ module Posto
     end
 
     def list(items)
-      print_all items
+      items
     end
 
     def run(items)
-      send(@arguments.command, items, *@arguments.params)
+      @io.puts send(@arguments.command, items, *@arguments.params)
     end
 
     def sort(items)
@@ -27,46 +27,40 @@ module Posto
     end
 
     def unsort(items, n = 1)
-      print_item(items, n)
       @file.write @list_utility.unsort(items, n.to_i)
+      lookup_item(items, n)
     end
 
     def done(items, n = 1)
-      print_item(items, n)
       @file.write @list_utility.done(items, n.to_i)
+      lookup_item(items, n)
     end
 
     def top(items, n = nil)
       if n.nil?
-        print_item(items)
+        lookup_item(items, 1)
       else
-        print_item(items, n)
         @file.write @list_utility.top(items, n.to_i)
+        lookup_item(items, n)
       end
     end
 
     def quick(items, n = 1)
-      items = @list_utility.quick(items, n.to_i)
-      print_all(items)
-      @file.write items
+      @file.write @list_utility.quick(items, n.to_i)
+      lookup_item(items, n)
     end
 
     def add(items, item)
-      items = @list_utility.add(items, item)
-      @io.puts items
-      @file.write items
+      @file.write @list_utility.add(items, item)
+      item
+    end
+
+    def lookup_item(items, n)
+      Posto::Item.hide_markdown(items[n - 1])
     end
 
     def method_missing(symbol, *args)
       STDERR.puts "Unsupported operation '#{symbol} #{args.join " "}'"
-    end
-
-    def print_item(items, n = 1)
-      @io.puts Posto::Item.hide_markdown(items[n - 1])
-    end
-
-    def print_all(items)
-      @io.puts items
     end
   end
 end
